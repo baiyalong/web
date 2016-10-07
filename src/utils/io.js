c0dc6be89965db4265403d3dccb9e976cf1771f6
bai <baiyalong@gmail.com>
2016-10-07 17:28:20 +0800

    conn

--- src/utils/io.js
+++ src/utils/io.js
@@ -7,9 +7,11 @@ var socket = io(server.addr)
 fingerprint(fp => {
     console.log(fp)
     socket.on('connect', () => {
-        socket.emit('user-connect', {
-            id: fp,
+        socket.emit('user.connect', {
+            fp,
             role: 'admin'
-        })
+        }, (err, user) => console.log('user.connect', err, user))
     })
+
+    socket.on('user.set', user => console.log('user.set', user))
 })
